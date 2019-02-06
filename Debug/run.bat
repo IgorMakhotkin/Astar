59e51e9c380508eab72a3b544414f06248ffb546
IgorMakhotkin <40770782+IgorMakhotkin@users.noreply.github.com>
2019-02-07 00:13:45 +0400

    fix

--- Debug/run.bat
+++ Debug/run.bat
@@ -1,5 +1,3 @@
-Astar.exe < data.txt > result.txt
-Astar.exe < data1.txt > result1.txt
-Astar.exe < data2.txt > result2.txt
-Astar.exe < data3.txt > result3.txt
+Astar.exe < map.txt  data.txt  >> result.txt
+
 	
